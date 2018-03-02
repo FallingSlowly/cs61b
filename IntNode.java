bd0d6708b5b9c3416d3c9ef7f0e773898ed37530
e <njautravis@gmail.com>
2018-03-02 13:42:36 +0800

    2.1 & 2.2 List

---
+++ IntNode.java
@@ -0,0 +1,13 @@
+/**
+ * IntNode
+ */
+public class IntNode {
+    public int item;
+    public IntNode next;
+
+    public IntNode(int n, IntNode n) {
+        item = i;
+        next = n;
+    }
+    
+}
