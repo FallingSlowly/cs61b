70a015d4a0d6661215b0b4d42757f99aae70d310
e <njautravis@gmail.com>
2018-03-08 10:49:48 +0800

    bug fix

--- TestSort.java
+++ TestSort.java
@@ -5,24 +5,17 @@ import java.util.Arrays;
  */
 public class TestSort {
     public static void testSort() {
-        Strict[] input = {"I", "have", "an", "egg"};
+        String[] input = {"I", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "I"};
 
         Sort.sort(input);
 
         for (int i = 0; i< input.length; i += 1) {
             if (!input[i].equals(expected[i])) {
-                System.out.println("Mismatch in position" + i + ", expected: " + expected[i] + ", but got: " + input[i]);
-            return;
+                System.out.println("Mismatch in position" + i + ", expected: " + expected[i] + ", but got: " + input[i] + ".");
+            break;
             }
         }
-
-        if (Arrays.equals(input, expected)) {
-            System.out.println("Error!");
-        }
-
-    }
-
 
     public static void main(String[] args) {
         testSort();
