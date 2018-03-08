32c64a2b705ada3bd5d9a6af2803783d43a4f61a
e <njautravis@gmail.com>
2018-03-08 15:00:33 +0800

    3.1 A New Way

--- TestSort.java
+++ TestSort.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import org.junit;
 
 /**
  * TestSort
@@ -10,14 +11,38 @@ public class TestSort {
 
         Sort.sort(input);
 
-        for (int i = 0; i< input.length; i += 1) {
-            if (!input[i].equals(expected[i])) {
-                System.out.println("Mismatch in position" + i + ", expected: " + expected[i] + ", but got: " + input[i] + ".");
-            break;
-            }
-        }
+        org.junit.Assert.assertEquals(expected, input);
+    }
+
+    public static void testFindSmallest() {
+        String[] input = {"I", "have", "an", "egg"};
+        int expected = 2;
+
+        int actual = Sort.findSmallest(input, 0);
+        org.junit.Assert.assertEquals(expected, actual);
+
+        String[] input2 = {"there", "are", "many", "pigs"};
+        int expected2 = 1;
+
+        int actual2 = Sort.findSmallest(input2, 2);
+        org.junit.Assert.assertEquals(expected2, actual2);
+    }
+
+    public static void testSwap() {
+        String[] input = {"I", "have", "an", "egg"};
+        
+        int a = 0;
+        int b = 2;
+
+        String[] expected = {"an", "egg", "have", "I"};
+
+        Sort.swap(input, a, b);
+
+        org.junit.Assert.assertEquals(expected, input);
+    }
 
     public static void main(String[] args) {
-        testSort();
+        testFindSmallest();
+        testSwap();
     }    
 }
